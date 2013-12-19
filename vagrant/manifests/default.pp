0739b3560ec144a6c8ae70d5fc5c627ecd6d29a8
leandrocr <leandrorepolho@gmail.com>
2013-12-19 18:25:05 -0200

    ajustes finais do manifesto

--- vagrant/manifests/default.pp
+++ vagrant/manifests/default.pp
@@ -12,14 +12,21 @@ service { "apache2":
 	require => Package["apache2"],
 }
 
+package { "mysql-server":
+	ensure  => present,
+	require => Exec["apt-get update"],
+}
+
+service { "mysql":
+	ensure  => "running",
+	require => Package["mysql-server"],
+}
+
 package { 'php5' :
   require => Package["apache2"],
 }
 
-class { '::mysql::server':
-  root_password    => '12345',
-  #override_options => { 'mysqld' => { 'max_connections' => '1024' } }
-}
+
 
 # file { "/var/www/sample-webapp":
 # 	ensure  => "link",
